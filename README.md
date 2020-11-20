607e54d263994d873ad8833c87b84f9ac2496cef
Thadeu José <thadeujs@gmail.com>
2020-11-19 23:43:36 -0300

    Commit all the files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tdd-by-example-test-case
 Code from TDD by example for the Test Case Example. Programming in Python.
+
+# How to run
+
+Open cmd in code folder
+
+Run this command 'python TestCase.py test/TestCaseTest.py'
---
+++ code/TestCaseTest.py
@@ -0,0 +1,47 @@
+from WasRun import WasRun
+from TestCase import TestCase
+from TestSuite import TestSuite
+from TestResult import TestResult
+
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
+    def testTemplateMethod(self):
+        test = WasRun('testMethod')
+        test.run(self.result)
+        assert test.log == 'setUp testMethod tearDown '
+
+    def testResult(self):
+        test = WasRun('testMethod')
+        result = test.run(self.result)
+        assert '1 run, 0 failed' == result.summary()
+
+    def testFailedResult(self):
+        test = WasRun('testBrokenMethod')
+        result = test.run(self.result)
+        assert '1 run, 1 failed' == result.summary()
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert '1 run, 1 failed' == result.summary()
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun('testMethod'))
+        suite.add(WasRun('testBrokenMethod'))
+        result = TestResult()
+        suite.run(result)
+        assert '2 run 1 failed' == result.summary()
+
+suite = TestSuite()
+suite.add(TestCaseTest('testTemplateMethod'))
+suite.add(TestCaseTest('testResult'))
+suite.add(TestCaseTest('testFailedResult'))
+suite.add(TestCaseTest('testFailedResultFormatting'))
+suite.add(TestCaseTest('testSuite'))
+result = TestResult()
+suite.run(result)
+print(result.summary())
